9f70f6c56605b568fe8dd74f9405d9ab379fa6c9
taylornlee2 <34076139+taylornlee2@users.noreply.github.com>
2019-01-16 09:15:09 -0800

    Delete Polygon.java

--- LeeTaylor_Polygon/Polygon.java
+++
@@ -1,8 +0,0 @@
-import java.awt.geom.*;
-public interface Polygon{
-   public void add(Point2D.Double aPoint);
-   public void draw();
-   public double perimeter();
-   public double area();
-   public String getName();
-}
